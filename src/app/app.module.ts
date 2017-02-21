9a258cea24e12ac93977b240f86722cede55588b
patricknasc <patrick.nascimento@gmail.com>
2017-02-21 04:17:41 -0300

    Bootstrap + Firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from 'angularfire2';
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyChfbiTaN7deX4Dyiggzv3vbq4LHqU50kw",
+  authDomain: "fir-crud-f765e.firebaseapp.com",
+  databaseURL: "https://fir-crud-f765e.firebaseio.com",
+  storageBucket: "fir-crud-f765e.appspot.com",
+  messagingSenderId: "450788208288"
+};
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
